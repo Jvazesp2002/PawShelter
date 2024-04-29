5b9e8a6b85dbce8735e5b2338f3d4a5fc9a71f8a
Jorge <jvazesp@g.educaand.es>
2024-04-29 16:23:28 +0200

    fixed routing

--- src-frontend/src/app/header/header.component.ts
+++ src-frontend/src/app/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon'
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
