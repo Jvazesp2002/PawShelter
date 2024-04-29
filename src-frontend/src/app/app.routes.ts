1fe2121093715fb997f582309425ff9095fab363
Jorge <jvazesp@g.educaand.es>
2024-04-29 17:49:51 +0200

    Added routes, all type user homepages, modified logo and login (still not working)

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { HomeComponent } from './home/home.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { VolunteerDashboardComponent } from './volunteer-dashboard/volunteer-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 
 export const routes: Routes = [
@@ -9,5 +12,7 @@ export const routes: Routes = [
   { path: 'registro', component: RegistroComponent },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-
+  { path: 'user', component: UserDashboardComponent },
+  { path: 'volunteer', component: VolunteerDashboardComponent },
+  { path: 'admin', component: AdminDashboardComponent }
 ];
