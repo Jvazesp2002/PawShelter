11206221f4995bf6569534ecb38c7f9b7fd4095f
Jorge <jvazesp@g.educaand.es>
2024-06-14 12:12:44 +0200

    Fixed layout problems

---
+++ src-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrlRef: 'http://ec2-54-235-55-145.compute-1.amazonaws.com:8080/api/refugio',
+    apiUrlAuth:'http://ec2-54-235-55-145.compute-1.amazonaws.com:8080/api/auth',
+    apiUrlGuar:'http://ec2-54-235-55-145.compute-1.amazonaws.com:8080/api/guarderia',
+};
